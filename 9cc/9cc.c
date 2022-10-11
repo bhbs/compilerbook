99cd3e7c1701cae8d33c9f0bd136db6367cb7ac0
taisei mima <bhbstar.me@gmail.com>
2022-10-11 16:34:40 +0000

    feat: error position

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -27,12 +27,19 @@ struct Token
 // 現在着目しているトークン
 Token *token;
 
-// エラーを報告するための関数
-// printfと同じ引数を取る
-void error(char *fmt, ...)
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...)
 {
   va_list ap;
   va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -53,7 +60,7 @@ bool consume(char op)
 void expect(char op)
 {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    error_at("'%c'ではありません", op);
   token = token->next;
 }
 
@@ -62,7 +69,7 @@ void expect(char op)
 int expect_number()
 {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -112,7 +119,7 @@ Token *tokenize(char *p)
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(token->str, "トークナイズできません");
   }
 
   new_token(TK_EOF, cur, p);
@@ -123,10 +130,11 @@ int main(int argc, char **argv)
 {
   if (argc != 2)
   {
-    error("引数の個数が正しくありません");
+    error_at(token->str, "引数の個数が正しくありません");
     return 1;
   }
 
+  user_input = argv[1];
   // トークナイズする
   token = tokenize(argv[1]);
 
